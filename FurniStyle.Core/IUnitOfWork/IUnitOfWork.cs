e29cf11cca1726cbdf9c7d5d6de9042dda3cf879
Hussein Adel <husseinadelhhh@gmail.com>
2025-03-22 14:16:26 +0200

    Adding UnitOfWork Design Pattern Implememtation

---
+++ FurniStyle.Core/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using FurniStyle.Core.Entities;
+using FurniStyle.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurniStyle.Core.IUnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync();
+        IGenericRepository<TEntity,TKey> Repository<TEntity,TKey>() where TEntity : BaseEntity<TKey>;  
+    }
+}
---
+++ FurniStyle.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using FurniStyle.Core.Entities;
+using FurniStyle.Core.IRepositories;
+using FurniStyle.Core.IUnitOfWork;
+using FurniStyle.Repository.Data.Context;
+using FurniStyle.Repository.Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurniStyle.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FurniStyleDbContext _furniStyleDbContext;
+        private Hashtable _repositories;
+
+        public UnitOfWork(FurniStyleDbContext furniStyleDbContext)
+        {
+            _furniStyleDbContext = furniStyleDbContext;
+            _repositories = new Hashtable();
+
+        }
+        public async Task<int> CompleteAsync()=> await _furniStyleDbContext.SaveChangesAsync();
+        
+
+        public IGenericRepository<TEntity, TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var type = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new GenericRepository<TEntity, TKey>(_furniStyleDbContext);
+                _repositories.Add(type, repository);
+            }
+            return _repositories[type] as IGenericRepository<TEntity, TKey>;
+        }
+    }
+}
